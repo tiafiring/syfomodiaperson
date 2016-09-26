d122a9c63db5f9ecbc4cc883ac46d00438d77639
Andreas Lund <andreas.lund@nav.no>
2016-09-26 14:12:46 +0200

    Sørger for at man får riktig URL til internarbeidsflatedecorator.

--- modiasyfofront-web/src/main/java/no/nav/sbl/modiasyfofront/SyfofrontDekorator.java
+++ modiasyfofront-web/src/main/java/no/nav/sbl/modiasyfofront/SyfofrontDekorator.java
@@ -6,10 +6,11 @@ import no.nav.innholdshenter.filter.DecoratorFilter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.System.getProperty;
 import static java.util.Arrays.asList;
 
 public class SyfofrontDekorator extends DecoratorFilter {
-    private static final String FRAGMENTS_URL = "/internarbeidsflatedecorator/headerv1.html";
+    private static final String FRAGMENTS_URL = "internarbeidsflatedecorator/headerv1.html";
     private static final String APPLICATION_NAME = "syfofront";
     private static final List<String> NO_DECORATOR_PATTERNS = new ArrayList<>(asList(".*/img/.*", ".*/css/.*", ".*/js/.*", ".*/font/.*", ".*selftest.*"));
     private static final List<String> FRAGMENT_NAMES= new ArrayList<>(asList("header-withmenu", "footer-withmenu", "styles"));
@@ -25,8 +26,7 @@ public class SyfofrontDekorator extends DecoratorFilter {
 
     private EnonicContentRetriever setUpContentRetriever() {
         EnonicContentRetriever contentRetriever = new EnonicContentRetriever(APPLICATION_NAME);
-        contentRetriever.setBaseUrl("http://localhost:8186");
-//        contentRetriever.setBaseUrl("https://d26jbsl00923.test.local:8443");
+        contentRetriever.setBaseUrl(getProperty("modapp.url"));
         contentRetriever.setRefreshIntervalSeconds(1800);
         contentRetriever.setHttpTimeoutMillis(10000);
         return contentRetriever;
