3c54f183592438a4872444c2cf7d41b51caaaa0b
Andreas Lund <andreas.lund@nav.no>
2016-09-30 08:51:07 +0200

    Legger inn ping mot syforestFss

--- modiasyfofront-web/src/main/java/no/nav/sbl/modiasyfofront/selftest/SelftestServlet.java
+++ modiasyfofront-web/src/main/java/no/nav/sbl/modiasyfofront/selftest/SelftestServlet.java
@@ -6,7 +6,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+import static java.lang.System.getProperty;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static java.util.Arrays.asList;
+import static no.nav.sbl.dialogarena.types.Pingable.Ping.*;
 
 public class SelftestServlet extends SelfTestBaseServlet{
     private static final String APPLIKASJONS_NAVN = "modiasyfofront";
@@ -24,6 +33,25 @@ public class SelftestServlet extends SelfTestBaseServlet{
 
     @Override
     protected Collection<? extends Pingable> getPingables() {
-        return ctx.getBeansOfType(Pingable.class).values();
+        return asList(
+                pingUrl("SYKEFRAVÆR_API", getProperty("sykefravaerapi.fss.url") + "/internal/isAlive", "application/json")
+        );
+    }
+
+    private Pingable pingUrl(final String name, final String url, final String expectedResponseType) {
+        return () -> {
+            HttpURLConnection connection;
+            try {
+                connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setConnectTimeout(10000);
+                if (!(connection.getResponseCode() == HTTP_OK && expectedResponseType != null) || connection.getContentType().startsWith(expectedResponseType)) {
+                    return lyktes(name);
+                } else {
+                    return feilet(name, new RuntimeException(connection.getResponseCode() + " " + connection.getResponseMessage()));
+                }
+            } catch (Exception e) {
+                return feilet(name, e);
+            }
+        };
     }
 }
