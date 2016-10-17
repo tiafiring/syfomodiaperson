2d677f41254731f3c0cac89473a52087cc6926a6
Andreas Lund <andreas.lund@nav.no>
2016-10-17 10:22:05 +0200

    Panel her også

--- modiasyfofront-web/src/frontend/js/components/ContextOppdatering.js
+++ modiasyfofront-web/src/frontend/js/components/ContextOppdatering.js
@@ -5,13 +5,15 @@ const ContextOppdatering = () => {
     return <div>
 
         <div className="panel" id="contextholderoppdatering" style={{display: 'none' }}>
-            <p>Hei! Nå oppdaterte vi brukeren her automatisk fordi du søkte på en ny bruker i Modia. Hva synes du om denne funksjonaliteten? Gi oss gjerne tilbakemelding om hvordan dette fungerer her: digisyfo@nav.no</p>
+            <p>Hei! Nå oppdaterte vi brukeren her automatisk fordi du søkte på en ny bruker i Modia. Hva synes du om denne funksjonaliteten?
+                Gi oss gjerne tilbakemelding om hvordan dette fungerer her: digisyfo@nav.no</p>
         </div>
 
 
 
-        <div id="contextholderfeil" style={{display: 'none' }}>
-            <p>Hei! Nå skjedde det en feil her. Da vil ikke brukeren du søker opp i Modia automatisk oppdateres her. </p>
+        <div className="panel" id="contextholderfeil" style={{display: 'none' }}>
+            <p>Hei! Nå skjedde det en feil her. Da vil ikke brukeren du søker opp i Modia automatisk oppdateres her. Skjer dette deg ofte?
+                Gi oss gjerne en tilbakemelding. Inkluder gjerne hva slags nettleser og versjon du bruker: digisyfo@nav.no</p>
         </div>
 
 
