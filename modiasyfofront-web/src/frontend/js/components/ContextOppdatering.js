e4698e282345175e36fb707a625bf3f0cd9a6aad
Andreas Lund <andreas.lund@nav.no>
2016-10-17 13:54:51 +0200

    Mailto lenke

--- modiasyfofront-web/src/frontend/js/components/ContextOppdatering.js
+++ modiasyfofront-web/src/frontend/js/components/ContextOppdatering.js
@@ -4,16 +4,17 @@ const ContextOppdatering = () => {
 
     return <div>
 
-        <div className="panel" id="contextholderoppdatering" style={{display: 'none' }}>
+        <div className="panel" id="contextholderoppdatering" style={{display: 'none' }} dangerouslySetInnerHTML={{ __html: innhold }}>
+
             <p>Hei! Nå oppdaterte vi brukeren her automatisk fordi du søkte på en ny bruker i Modia. Hva synes du om denne funksjonaliteten?
-                Gi oss gjerne tilbakemelding om hvordan dette fungerer her: digisyfo@nav.no</p>
+                Gi oss gjerne tilbakemelding om hvordan dette fungerer her: <a href="mailto:digisyfo@nav.no?Subject=Veileder%20Tilbakemelding" target="_top">Tilbakemelding</a></p>
         </div>
 
 
 
         <div className="panel" id="contextholderfeil" style={{display: 'none' }}>
             <p>Hei! Nå skjedde det en feil her. Da vil ikke brukeren du søker opp i Modia automatisk oppdateres her. Skjer dette deg ofte?
-                Gi oss gjerne en tilbakemelding. Inkluder gjerne hva slags nettleser og versjon du bruker: digisyfo@nav.no</p>
+                Gi oss gjerne en tilbakemelding. Inkluder gjerne hva slags nettleser og versjon du bruker: <a href="mailto:digisyfo@nav.no?Subject=Veileder%20Tilbakemelding" target="_top">Tilbakemelding</a></p>
         </div>
 
 
