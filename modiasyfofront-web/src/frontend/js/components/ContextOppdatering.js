2d256803d802472b4af61ee26d73ba6d0afa3137
Andreas Lund <andreas.lund@nav.no>
2016-10-17 13:59:49 +0200

    fjerner overføldig attribut.

--- modiasyfofront-web/src/frontend/js/components/ContextOppdatering.js
+++ modiasyfofront-web/src/frontend/js/components/ContextOppdatering.js
@@ -4,7 +4,7 @@ const ContextOppdatering = () => {
 
     return <div>
 
-        <div className="panel" id="contextholderoppdatering" style={{display: 'none' }} dangerouslySetInnerHTML={{ __html: innhold }}>
+        <div className="panel" id="contextholderoppdatering" style={{display: 'none' }}>
 
             <p>Hei! Nå oppdaterte vi brukeren her automatisk fordi du søkte på en ny bruker i Modia. Hva synes du om denne funksjonaliteten?
                 Gi oss gjerne tilbakemelding om hvordan dette fungerer her: <a href="mailto:digisyfo@nav.no?Subject=Veileder%20Tilbakemelding" target="_top">Tilbakemelding</a></p>
