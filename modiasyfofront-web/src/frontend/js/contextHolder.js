7034b8512b09536f7ef483b79b306317cb1b0acd
Andreas Lund <andreas.lund@nav.no>
2016-10-19 14:37:38 +0200

    git merge bumper versjoner.

--- modiasyfofront-web/src/frontend/js/contextHolder.js
+++ modiasyfofront-web/src/frontend/js/contextHolder.js
@@ -6,7 +6,7 @@ const getPort = () => {
 };
 
 const ContextholderConnection = (brukerId) => {
-    const webSocketUrl = `${window.SYFO_SETTINGS.WEBSOCKET_PROTOCOL}://${window.location.hostname}${getPort()}/contextholder/websocket/${brukerId}`;
+    const webSocketUrl = `${window.SYFO_SETTINGS.WEBSOCKET_PROTOCOL}://${window.location.hostname}${getPort()}/eventdistributer/websocket/${brukerId}`;
     return new WebSocket(webSocketUrl);
 };
 
