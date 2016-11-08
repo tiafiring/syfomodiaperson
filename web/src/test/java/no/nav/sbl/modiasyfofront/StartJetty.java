2d53f7c9f74138c8f3956e7e84dc62ad8a91a512
Andreas Lund <andreas.lund@nav.no>
2016-11-08 09:22:17 +0100

    Legger frontapplikasjonen bak SPNEGO.

--- web/src/test/java/no/nav/sbl/modiasyfofront/StartJetty.java
+++ web/src/test/java/no/nav/sbl/modiasyfofront/StartJetty.java
@@ -1,23 +1,38 @@
 package no.nav.sbl.modiasyfofront;
 
+import no.nav.modig.core.context.StaticSubjectHandler;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
+import org.eclipse.jetty.jaas.JAASLoginService;
 
+import static java.lang.System.setProperty;
+import static no.nav.modig.core.context.SubjectHandler.SUBJECTHANDLER_KEY;
 import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
 import static no.nav.modig.lang.collections.RunnableUtils.first;
 import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
+import no.nav.modig.security.loginmodule.DummyRole;
 
 public class StartJetty {
     private static final int PORT = 8190;
 
     public static void main(String[] args) throws Exception {
+        setProperty(SUBJECTHANDLER_KEY, StaticSubjectHandler.class.getName());
+
+
         Jetty jetty = usingWar()
                 .at("/sykefravaer")
                 .overrideWebXml()
+                .withLoginService(createLoginService())
                 .loadProperties("/test.properties")
                 .port(PORT)
                 .buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
     }
 
+    public static JAASLoginService createLoginService() {
+        JAASLoginService jaasLoginService = new JAASLoginService("Simple Login Realm");
+        jaasLoginService.setLoginModuleName("simplelogin");
+        jaasLoginService.setRoleClassNames(new String[]{DummyRole.class.getName()});
+        return jaasLoginService;
+    }
 }
