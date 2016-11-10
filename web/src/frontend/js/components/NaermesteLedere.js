cfb587b4695039825463e7c3d4048baf8c806ab0
Helge Fredheim <helge.fredheim@nav.no>
2016-11-10 10:18:14 +0100

    Begynte på nytt design. Gjenstår ikoner + håndtering av erInnmeldt

--- web/src/frontend/js/components/NaermesteLedere.js
+++ web/src/frontend/js/components/NaermesteLedere.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import Sidetopp from './Sidetopp';
 
 const Leder = ({ arbeidsgiver, epost, navn, tlf, fodselsdato }) => {
-    return (<div className="rad--adskilt naermesteLeder js-leder">
+    return (<div className="naermesteLeder js-leder">
             <div className="grid">
                 <div className="unit full">
                     <h3>{arbeidsgiver.navn}</h3>
@@ -13,23 +13,23 @@ const Leder = ({ arbeidsgiver, epost, navn, tlf, fodselsdato }) => {
                     <h4>Navn</h4>
                     <p>{navn}</p>
                 </div>
+                <div className="unit two-thirds">
+                    <h4>E-post</h4>
+                    <p><a href={`mailto:${epost}`}>{epost}</a></p>
+                </div>
+            </div>
+            <div className="grid">
                 <div className="unit one-third">
                     <h4>Telefon</h4>
                     <p>{tlf}</p>
                 </div>
                 <div className="unit one-third">
+                    <h4>Meldt inn</h4>
+                    <p>KOMMER HER</p>
+                </div>
+                <div className="unit one-third">
                     <h4>Org. nummer</h4>
                     <p>{arbeidsgiver.orgnummer}</p>
-                </div>
-            </div>
-            <div className="grid">
-                <div className="unit one-third">
-                    <h4>E-post</h4>
-                    <p><a href={`mailto:${epost}`}>{epost}</a></p>
-                </div>
-                <div className="unit one-third">
-                    <h4>Fødselsdato</h4>
-                    <p>{fodselsdato}</p>
                 </div>
             </div>
         </div>
@@ -48,7 +48,7 @@ const NaermesteLedere = ({ ledere = [], navbruker }) => {
     const tittel = ledere.length > 1 ? 'Nærmeste ledere med personalansvar' : 'Nærmeste leder med personalansvar';
     return (<div className="panel">
         <Sidetopp tittel={tittel} />
-        <p>*kan kontaktes av NAV i forbindelse med oppfølging</p>
+        <p className="blokk--l">Opplysninger under brukes i forbindelse med oppfølging</p>
         <div className="naermesteLedere">
             {
                 ledere.length === 0 && <p>Nærmeste leder med personalansvar for {navbruker.navn} er ikke oppgitt.</p>
