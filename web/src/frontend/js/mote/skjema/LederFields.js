7b9bbccf6d370d483a73468019037fba74f46d0c
Helge Fredheim <helge.fredheim@nav.no>
2016-12-15 16:27:01 +0100

    Litt refaktorering - flyttet ArbeidsgiverDropdown over i en egen fil

--- web/src/frontend/js/mote/skjema/LederFields.js
+++ web/src/frontend/js/mote/skjema/LederFields.js
@@ -3,59 +3,7 @@ import { Field } from 'redux-form';
 import TextField from '../../components/TextField';
 import TextFieldLocked from '../../components/TextFieldLocked';
 import FyllUtVirksomhet from './FyllUtVirksomhet';
-
-export const ArbeidsgiverDropdown = ({ meta, input, ledere }) => {
-    return (<div className="blokk--xl">
-        <label htmlFor="velg-arbeidsgiver">Velg arbeidsgiver</label>
-        <div className="selectContainer input--xxl">
-            <select className={meta.touched && meta.error ? 'input--feil' : ''} id="velg-arbeidsgiver" {...input}>
-                <option value="VELG">Velg arbeidsgiver</option>
-                {
-                    ledere
-                    .sort((a, b) => {
-                        if (a.navn > b.navn) {
-                            return 1;
-                        } else if (b.navn > a.navn) {
-                            return -1;
-                        }
-                        return 0;
-                    })
-                    .sort((a, b) => {
-                        if (a.organisasjonsnavn > b.organisasjonsnavn) {
-                            return 1;
-                        } else if (b.organisasjonsnavn > a.organisasjonsnavn) {
-                            return -1;
-                        }
-                        return 0;
-                    })
-                    .map((leder, idx) => {
-                        return <option value={leder.id} key={idx}>{leder.organisasjonsnavn}</option>;
-                    })
-                }
-                <option value="manuell">Ikke oppgitt &ndash; fyll inn manuelt</option>
-            </select>
-        </div>
-        <p className="skjema__feilmelding" aria-live="polite">{meta.touched && meta.error}</p>
-    </div>);
-};
-
-ArbeidsgiverDropdown.propTypes = {
-    meta: PropTypes.object,
-    input: PropTypes.object,
-    virksomhet: PropTypes.object,
-    ledere: PropTypes.array,
-};
-
-export const PreutfyltLeder = () => {
-    return <FyllUtLeder FieldComponent={TextFieldLocked} />;
-};
-
-export const ManuellUtfyltLeder = () => {
-    return (<div>
-        <FyllUtLeder />
-        <FyllUtVirksomhet />
-    </div>);
-};
+import ArbeidsgiverDropdown from './ArbeidsgiverDropdown';
 
 export const FyllUtLeder = ({ FieldComponent = TextField }) => {
     return (<div>
@@ -72,6 +20,17 @@ export const FyllUtLeder = ({ FieldComponent = TextField }) => {
 
 FyllUtLeder.propTypes = {
     FieldComponent: PropTypes.func,
+};
+
+export const PreutfyltLeder = () => {
+    return <FyllUtLeder FieldComponent={TextFieldLocked} />;
+};
+
+export const ManuellUtfyltLeder = () => {
+    return (<div>
+        <FyllUtLeder />
+        <FyllUtVirksomhet />
+    </div>);
 };
 
 export default class LederFields extends Component {
--- web/src/frontend/test/mote/skjema/LederFieldsTest.js
+++ web/src/frontend/test/mote/skjema/LederFieldsTest.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import LederFields, { VelgArbeidsgiver, FyllUtLeder, ArbeidsgiverDropdown, ManuellUtfyltLeder, PreutfyltLeder } from '../../../js/mote/skjema/LederFields';
+import LederFields, { VelgArbeidsgiver, FyllUtLeder, ManuellUtfyltLeder, PreutfyltLeder } from '../../../js/mote/skjema/LederFields';
+import ArbeidsgiverDropdown from '../../../js/mote/skjema/ArbeidsgiverDropdown';
 import FyllUtVirksomhet from '../../../js/mote/skjema/FyllUtVirksomhet';
 import TextFieldLocked from '../../../js/components/TextFieldLocked';
 import TextField from '../../../js/components/TextField';
@@ -156,38 +157,4 @@ describe("LederFields", () => {
             expect(spy.calledOnce).to.be.false;
         });
     })
-})
-
-describe("ArbeidsgiverDropdown", () => {
-
-    let meta;
-    let ledere;
-
-    beforeEach(() => {
-        meta = {}
-        ledere = [{
-            id: 1,
-            navn: "Bjarne",
-            organisasjonsnavn: "BEKK"
-        }, {
-            id: 2,
-            navn: "Arne", 
-            organisasjonsnavn: "BEKK"
-        }, {
-            id: 3, 
-            navn: "Abba",
-            organisasjonsnavn: "BEKK"
-        }, {
-            id: 4,
-            navn: "Aage",
-            organisasjonsnavn: "ABC Buss"
-        }]
-    });
-
-    it("Skal inneholde et valg for manuell utfylling", () => {
-        const compo = mount(<ArbeidsgiverDropdown meta={meta} ledere={ledere} />);
-        expect(compo.find("option")).to.have.length(6);
-        expect(compo.find("option").last().text()).to.contain("fyll inn manuelt")
-    });
-
 });
