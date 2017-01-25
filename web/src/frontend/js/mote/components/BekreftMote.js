3679dac2deb6f21c8d57e3ae63b8c2a44f734c04
Andreas Lund <andreas.lund@nav.no>
2017-01-25 08:49:22 +0100

    Tid og sted på bekreftMote popupen.

--- web/src/frontend/js/mote/components/BekreftMote.js
+++ web/src/frontend/js/mote/components/BekreftMote.js
@@ -1,10 +1,21 @@
 import React, {PropTypes} from "react";
 import {Link} from "react-router";
-import {fikkMoteOpprettetVarsel} from "../utils/index";
+import {fikkMoteOpprettetVarsel, getTidFraZulu} from "../utils/index";
+import {hentDag} from "../../utils/index";
 
-const BekreftMote = ({ deltaker, sykmeldtDeltaker, onSubmit, avbrytHref }) => {
+const BekreftMote = ({ deltaker, sykmeldtDeltaker, onSubmit, avbrytHref, alternativ }) => {
     return (<div className="epostinnhold">
-        <h2 className="typo-innholdstittel">Send møteresultat</h2>
+        <h2 className="typo-innholdstittel">Send bekreftelse på møte</h2>
+
+        <div className="epostinnhold__mottakere blokk">
+            <h3>Dato og tid</h3>
+            <p>{hentDag(alternativ.tid)} {getTidFraZulu(alternativ.tid)}</p>
+        </div>
+
+        <div className="epostinnhold__mottakere blokk epostinnhold_avgrensning_bunn">
+            <h3>Møtested</h3>
+            <p>{alternativ.sted}</p>
+        </div>
 
         <div className="epostinnhold__mottakere blokk">
             <h3>Sendes til arbeidsgiver</h3>
