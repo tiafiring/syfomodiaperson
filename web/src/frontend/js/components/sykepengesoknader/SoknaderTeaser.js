1174be8b5e5f3805cfb81fa462e3af6e55efea0b
Andreas Lund <andreas.lund@nav.no>
2017-08-21 10:47:45 +0200

    Ikoner i IE...

--- web/src/frontend/js/components/sykepengesoknader/SoknaderTeaser.js
+++ web/src/frontend/js/components/sykepengesoknader/SoknaderTeaser.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { getLedetekst, toDatePrettyPrint } from 'digisyfo-npm';
 import { sykepengesoknad as sykepengesoknadPt } from '../../propTypes';
@@ -42,89 +42,62 @@ export const getSendtTilSuffix = (sykepengesoknad) => {
     return '';
 };
 
-class SoknadTeaser extends Component {
+const SoknadTeaser = ({ sykepengesoknad, fnr }) => {
+    const visStatus = sykepengesoknad.status !== NY && sykepengesoknad.status !== SENDT;
+    const sendtTilBeggeMenIkkeSamtidig = erSendtTilBeggeMenIkkeSamtidig(sykepengesoknad);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            ikon: 'soknader.svg',
-        };
-    }
-
-    onMouseEnter() {
-        this.setState({
-            ikon: 'soknader_hover-blue.svg',
-        });
-    }
-
-    onMouseLeave() {
-        this.setState({
-            ikon: 'soknader.svg',
-        });
-    }
-
-    render() {
-        const { sykepengesoknad, fnr } = this.props;
-
-        const visStatus = sykepengesoknad.status !== NY && sykepengesoknad.status !== SENDT;
-        const sendtTilBeggeMenIkkeSamtidig = erSendtTilBeggeMenIkkeSamtidig(sykepengesoknad);
-
-        return (<article aria-labelledby={`soknader-header-${sykepengesoknad.id}`}>
-            <Link className="inngangspanel js-panel" to={`/sykefravaer/${fnr}/sykepengesoknader/${sykepengesoknad.id}`}
-                onMouseEnter={() => {this.onMouseEnter();}}
-                onMouseLeave={() => {this.onMouseLeave();}}
-            >
-                <span className="inngangspanel__ikon">
-                    <img className="js-ikon" src={`/sykefravaer/img/svg/${this.state.ikon}`} />
-                </span>
-                <div className="inngangspanel__innhold">
-                    <header className="inngangspanel__header">
-                        <h3 className="js-title" id={`soknad-header-${sykepengesoknad.id}`}>
-                            <small className="inngangspanel__meta js-meta">
-                                {getLedetekst('soknad.teaser.dato', { '%DATO%': toDatePrettyPrint(sykepengesoknad.opprettetDato) }) }
-                            </small>
-                            <span className="inngangspanel__tittel">
-                                {getLedetekst('soknad.teaser.tittel')}
-                            </span>
-                        </h3>
-                        {
-                            visStatus &&
-                            <p className="inngangspanel__status js-status">
-                                {
-                                    getLedetekst(`soknad.teaser.status.${sykepengesoknad.status}`, {
-                                        '%DATO%': toDatePrettyPrint(sykepengesoknad.sendtTilArbeidsgiverDato || sykepengesoknad.sendtTilNAVDato),
-                                    })
-                                }
-                            </p>
-                        }
-                    </header>
-                    <p className="inngangspanel__tekst js-tekst">
-                        {
-                            getLedetekst('soknad.teaser.tekst', {
-                                '%FRA%': toDatePrettyPrint(sykepengesoknad.fom),
-                                '%TIL%': toDatePrettyPrint(sykepengesoknad.tom),
-                            })
-                        }
-                    </p>
-                    <p className="inngangspanel__undertekst js-undertekst mute">
-                        {
-                            sykepengesoknad.status !== SENDT && sykepengesoknad.status !== TIL_SENDING && getLedetekst('soknad.teaser.undertekst', { '%ARBEIDSGIVER%': sykepengesoknad.arbeidsgiver.navn })
-                        }
-                        {
-                            sendtTilBeggeMenIkkeSamtidig && sykepengesoknad.status !== NY && <SendtUlikt sykepengesoknad={sykepengesoknad} />
-                        }
-                        {
-                            !sendtTilBeggeMenIkkeSamtidig && sykepengesoknad.status !== NY && sykepengesoknad.status !== UTKAST_TIL_KORRIGERING && getLedetekst(`soknad.teaser.status.${sykepengesoknad.status}${getSendtTilSuffix(sykepengesoknad)}`, {
-                                '%DATO%': toDatePrettyPrint(sykepengesoknad.sendtTilArbeidsgiverDato || sykepengesoknad.sendtTilNAVDato),
-                                '%ARBEIDSGIVER%': sykepengesoknad.arbeidsgiver.navn,
-                            })
-                        }
-                    </p>
-                </div>
-            </Link>
-        </article>);
-    }
-}
+    return (<article aria-labelledby={`soknader-header-${sykepengesoknad.id}`}>
+        <Link className="inngangspanel js-panel" to={`/sykefravaer/${fnr}/sykepengesoknader/${sykepengesoknad.id}`}
+            onMouseEnter={() => {this.onMouseEnter();}}
+            onMouseLeave={() => {this.onMouseLeave();}}
+        >
+            <div className="inngangspanel__innhold">
+                <header className="inngangspanel__header">
+                    <h3 className="js-title" id={`soknad-header-${sykepengesoknad.id}`}>
+                        <small className="inngangspanel__meta js-meta">
+                            {getLedetekst('soknad.teaser.dato', { '%DATO%': toDatePrettyPrint(sykepengesoknad.opprettetDato) }) }
+                        </small>
+                        <span className="inngangspanel__tittel">
+                            {getLedetekst('soknad.teaser.tittel')}
+                        </span>
+                    </h3>
+                    {
+                        visStatus &&
+                        <p className="inngangspanel__status js-status">
+                            {
+                                getLedetekst(`soknad.teaser.status.${sykepengesoknad.status}`, {
+                                    '%DATO%': toDatePrettyPrint(sykepengesoknad.sendtTilArbeidsgiverDato || sykepengesoknad.sendtTilNAVDato),
+                                })
+                            }
+                        </p>
+                    }
+                </header>
+                <p className="inngangspanel__tekst js-tekst">
+                    {
+                        getLedetekst('soknad.teaser.tekst', {
+                            '%FRA%': toDatePrettyPrint(sykepengesoknad.fom),
+                            '%TIL%': toDatePrettyPrint(sykepengesoknad.tom),
+                        })
+                    }
+                </p>
+                <p className="inngangspanel__undertekst js-undertekst mute">
+                    {
+                        sykepengesoknad.status !== SENDT && sykepengesoknad.status !== TIL_SENDING && getLedetekst('soknad.teaser.undertekst', { '%ARBEIDSGIVER%': sykepengesoknad.arbeidsgiver.navn })
+                    }
+                    {
+                        sendtTilBeggeMenIkkeSamtidig && sykepengesoknad.status !== NY && <SendtUlikt sykepengesoknad={sykepengesoknad} />
+                    }
+                    {
+                        !sendtTilBeggeMenIkkeSamtidig && sykepengesoknad.status !== NY && sykepengesoknad.status !== UTKAST_TIL_KORRIGERING && getLedetekst(`soknad.teaser.status.${sykepengesoknad.status}${getSendtTilSuffix(sykepengesoknad)}`, {
+                            '%DATO%': toDatePrettyPrint(sykepengesoknad.sendtTilArbeidsgiverDato || sykepengesoknad.sendtTilNAVDato),
+                            '%ARBEIDSGIVER%': sykepengesoknad.arbeidsgiver.navn,
+                        })
+                    }
+                </p>
+            </div>
+        </Link>
+    </article>);
+};
 
 
 SoknadTeaser.propTypes = {
