f4a0960d45149bab0f8d43934455954d24b7d0d1
Erik Jansen <erik.gunnar.jansen@nav.no>
2017-09-19 09:23:17 +0200

    Fiks lint error: unused import

--- web/src/frontend/js/sykmeldinger/sykmelding/DinSykmelding.js
+++ web/src/frontend/js/sykmeldinger/sykmelding/DinSykmelding.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import { Varselstripe, DineSykmeldingOpplysninger, getLedetekst, getHtmlLedetekst } from 'digisyfo-npm';
+import { Varselstripe, DineSykmeldingOpplysninger, getLedetekst } from 'digisyfo-npm';
 import IllustrertInnhold from '../../components/IllustrertInnhold';
 
 const DinSykmelding = ({ sykmelding, ledetekster, visEldreSykmeldingVarsel, eldsteSykmeldingId }) => {
