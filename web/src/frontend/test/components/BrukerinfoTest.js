048ff49afcac01bedf5fdefb18a1ef2f33335925
Andreas Lund <andreas.lund@nav.no>
2017-11-15 07:50:05 +0100

    tester.

--- web/src/frontend/test/components/BrukerinfoTest.js
+++ web/src/frontend/test/components/BrukerinfoTest.js
@@ -8,15 +8,20 @@ describe("Brukerinfo", () => {
     const navbruker = {
         navn: "Knut",
         fnr: "1234"
-    }
+    };
+
+    const behandlendeEnhet = {
+        navn: "NAV Drammen",
+        enhetId: "1234"
+    };
 
     it("Skal vise brukers personnummer", () => {
-        const component = render(<Brukerinfo navbruker={navbruker} />);
+        const component = render(<Brukerinfo navbruker={navbruker} behandlendeEnhet={behandlendeEnhet} />);
         expect(component.text()).to.contain("1234");
     });
 
     it("Skal vise brukers navn", () => {
-        const component = render(<Brukerinfo navbruker={navbruker} />);
+        const component = render(<Brukerinfo navbruker={navbruker} behandlendeEnhet={behandlendeEnhet} />);
         expect(component.text()).to.contain("Knut");
     });
 
