4c3de86b81aefef47a5aa3d8b546d7c944f422d1
Andreas Lund <andreas.lund@nav.no>
2017-11-21 11:27:20 +0100

    Litt tettere på panelet her.

--- web/src/frontend/js/components/oppfoelgingsdialoger/IngenPlaner.js
+++ web/src/frontend/js/components/oppfoelgingsdialoger/IngenPlaner.js
@@ -4,7 +4,7 @@ import Infomelding from '../Infomelding';
 const IngenPlaner = () => {
     return (<div>
         <div className="panel">
-            <h2>Oppfølgingsplaner</h2>
+            <h2 style={{ margin: 0 }}>Oppfølgingsplaner</h2>
         </div>
         <div className="panel">
             <Infomelding tittel="Du har ikke mottatt noen oppfølgingsplaner" melding="For å se oppfølgingsplaner her må den sykmeldte eller nærmeste leder sende den inn fra sin side på nav.no" />
