99e6a4fdb60b300b6fd28fafae58adf357c3d1a7
Andreas Lund <andreas.lund@nav.no>
2017-11-26 09:59:44 +0100

    Ved ingen historikk skal det hete Logg, ikke Historikk.

--- web/src/frontend/js/components/historikk/IngenHistorikk.js
+++ web/src/frontend/js/components/historikk/IngenHistorikk.js
@@ -4,7 +4,7 @@ import Infomelding from '../Infomelding';
 const IngenHistorikk = () => {
     return (<div>
         <div className="panel">
-            <h2 style={{ margin: 0 }}>Historikk</h2>
+            <h2 style={{ margin: 0 }}>Logg</h2>
         </div>
         <div className="panel">
             <Infomelding tittel="Denne personen har ingen oppfølgingshistorikk" melding="Når en sykmeldt blir fulgt opp så vil oppfølgingen bli loggført her slik at du får oversikt over hva som har skjedd og hvem som har vært involvert i oppfølgingen." />
