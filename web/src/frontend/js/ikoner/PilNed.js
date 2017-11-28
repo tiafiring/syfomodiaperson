fc7700f987be1d58f9a4c42794ee53d408639d2a
Andreas Lund <andreas.lund@nav.no>
2017-11-28 07:59:43 +0100

    gjør pilene litt mindre.

--- web/src/frontend/js/ikoner/PilNed.js
+++ web/src/frontend/js/ikoner/PilNed.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PilNed = ({ farge }) => {
-    return (<svg style={{ flex: 1 }} width="20" height="20" viewBox="0 0 15.15 9">
+    return (<svg style={{ flex: 1 }} width="14" height="14" viewBox="0 0 15.15 9">
             <path style={{ fill:'none', stroke:farge, strokeLinecap:'round', strokeLinejoin: 'round' }} d="M.5.5l7.08 8 7.07-8" />
         </svg>
     );
--- web/src/frontend/js/ikoner/PilOpp.js
+++ web/src/frontend/js/ikoner/PilOpp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PilOpp = ({ farge }) => {
-    return (<svg style={{ flex: 1 }} width="20" height="20" viewBox='0 0 15.15 9'>
+    return (<svg style={{ flex: 1 }} width="14" height="14" viewBox='0 0 15.15 9'>
             <path style={{ fill:'none', stroke:farge, strokeLinecap:'round', strokeLinejoin: 'round' }} d='M14.65 8.5L7.58.5.5 8.5' />
         </svg>
     );
