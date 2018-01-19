7bedf52b46912b819c331e8a0c8fc312356a6a69
Andreas Lund <andreas.lund@nav.no>
2018-01-19 09:27:46 +0100

    Bytter openamagent

--- web/src/test/java/no/nav/sbl/modiasyfofront/StartJetty.java
+++ web/src/test/java/no/nav/sbl/modiasyfofront/StartJetty.java
@@ -1,35 +1,24 @@
 package no.nav.sbl.modiasyfofront;
 
-import no.nav.modig.core.context.StaticSubjectHandler;
+import no.nav.brukerdialog.security.context.CustomizableSubjectHandler;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
-import org.apache.geronimo.components.jaspi.AuthConfigFactoryImpl;
-import org.eclipse.jetty.jaas.JAASLoginService;
 
 import static java.lang.System.setProperty;
-import static no.nav.modig.core.context.SubjectHandler.SUBJECTHANDLER_KEY;
-import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
-import static no.nav.modig.lang.collections.RunnableUtils.first;
-import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
-import no.nav.modig.security.loginmodule.DummyRole;
-
-import javax.security.auth.message.config.AuthConfigFactory;
-import java.security.Security;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.first;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.gotKeypress;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.waitFor;
 
 public class StartJetty {
     private static final int PORT = 8190;
 
     public static void main(String[] args) throws Exception {
-        setProperty(SUBJECTHANDLER_KEY, StaticSubjectHandler.class.getName());
-        setProperty("org.apache.geronimo.jaspic.configurationFile", "src/test/resources/jaspiconf.xml");
-        Security.setProperty(AuthConfigFactory.DEFAULT_FACTORY_SECURITY_PROPERTY, AuthConfigFactoryImpl.class.getCanonicalName());
-
+        setProperty("no.nav.brukerdialog.security.context.subjectHandlerImplementationClass", CustomizableSubjectHandler.class.getName());
 
         Jetty jetty = usingWar()
                 .at("/sykefravaer")
                 .overrideWebXml()
                 .loadProperties("/test.properties")
-                .configureForJaspic()
                 .port(PORT)
                 .buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
