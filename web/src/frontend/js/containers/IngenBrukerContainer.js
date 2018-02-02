f8b9f494f4aecf6232afcd76c971a26205b33017
Andreas Lund <andreas.lund@nav.no>
2018-02-02 11:46:14 +0100

    Infomelding istedenfor feilmelding.

--- web/src/frontend/js/containers/IngenBrukerContainer.js
+++ web/src/frontend/js/containers/IngenBrukerContainer.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Feilmelding from '../components/Feilmelding';
+import Infomelding from '../components/Infomelding';
 
 export const IngenBrukerSide = () => {
     return (
         <div >
-            <Feilmelding tittel="Du har ikke valgt hvilken person du ønsker å se" melding={{ __html: "<div>" +
-            "<p>For å se oppfølgingen må du skrive inn fødselsnummeret i menylinjen. </p>" +
-            "</div>" }} />
+            <Infomelding tittel="Du har ikke valgt hvilken person du ønsker å se" melding="For å se oppfølgingen må du skrive inn fødselsnummeret i menylinjen" />
         </div>
     );
 };
