b0b5f5c29d6e52215612a5a9ecdcded3e0ced509
Andreas Lund <andreas.lund@nav.no>
2018-02-14 16:19:28 +0100

    vil at HENT_SYKEFORLOEP_FEILET skal throwes herfra.

--- web/src/frontend/js/sagas/sykeforloepSagas.js
+++ web/src/frontend/js/sagas/sykeforloepSagas.js
@@ -1,12 +1,12 @@
 import { call, put, fork } from 'redux-saga/effects';
 import { takeEvery } from 'redux-saga';
-import { getWithoutThrows } from '../api/index';
+import { get } from '../api/index';
 import { log } from 'digisyfo-npm';
 
 export function* hentSykeforloep(action) {
     yield put({ type: 'HENTER_SYKEFORLOEP' });
     try {
-        const data = yield call(getWithoutThrows, `${window.APP_SETTINGS.REST_ROOT}/sykeforloep?fnr=${action.fnr}`);
+        const data = yield call(get, `${window.APP_SETTINGS.REST_ROOT}/sykeforloep?fnr=${action.fnr}`);
         yield put({ type: 'SYKEFORLOEP_HENTET', data });
     } catch (e) {
         log(e);
