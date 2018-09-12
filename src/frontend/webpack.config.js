17de5b7184960e47994cc627c26b5091938a6898
Helge Fredheim <helge.fredheim@nav.no>
2018-09-12 09:25:49 +0200

    Bygger med webpack 4

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,42 +1,62 @@
-var path = require("path");
-var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-var stylesPath = path.resolve(__dirname, 'styles', 'styles.less');
+var path = require('path');
 var mainPath = path.resolve(__dirname, 'js', 'index.js');
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
-    entry: [mainPath, stylesPath],
+    entry: ['babel-polyfill', mainPath],
     output: {
         path: path.resolve(__dirname, "build"),
         publicPath: "http://localhost:3040/assets/",
         filename: "bundle.js"
     },
+    mode: 'development',
+    resolve: {
+        alias: {
+            react: path.join(__dirname, 'node_modules', 'react'),
+        },
+    },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ["react", "env"]
-                },
-                exclude: [nodeModulesPath]
-            },
+        rules: [
             {
                 test: /\.less$/,
-                loaders: ['style-loader', 'css-loader', 'less-loader?{"globalVars":{"nodeModulesPath":"\'~\'", "coreModulePath":"\'~\'"}}']
+                use: [{
+                    loader: 'style-loader',
+                }, {
+                    loader: 'css-loader',
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: function() {
+                            return [autoprefixer];
+                        },
+                    },
+                }, {
+                    loader: 'less-loader',
+                    options: {
+                        globalVars: {
+                            nodeModulesPath: '~',
+                            coreModulePath: '~',
+                        },
+                    },
+                }],
             },
             {
-                test: /\.json$/, 
-                loader: 'json' 
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'env', 'babel-preset-stage-0'],
+                    },
+                }],
             },
-            { 
-                test: /\.((woff2?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|svg|jpe?g|png|gif|ico)$/, 
-                loader: 'url?limit=10000' 
+            {
+                test: /\.((woff2?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|svg|jpe?g|png|gif|ico)$/,
+                use: [{
+                    loader: 'svg-url-loader',
+                }],
             },
-            {  
-                test: /\.((ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9]))|(ttf|eot)$/,
-                loader: 'file' 
-            }
-        ]
+        ],
     },
     devServer: {
         stats: 'errors-only',
