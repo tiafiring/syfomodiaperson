78e5e99efadb07d9867f4a09afdeda817abe50eb
JMLindseth-nav <40055758+JMLindseth-nav@users.noreply.github.com>
2019-02-08 15:45:25 +0100

    Fiks endepunkt-url i oppfolgingstilfelleperioderSagas
    SYFOUTV-2651

--- src/frontend/js/sagas/oppfolgingstilfelleperioderSagas.js
+++ src/frontend/js/sagas/oppfolgingstilfelleperioderSagas.js
@@ -12,7 +12,7 @@ import { HENT_OPPFOLGINGSTILFELLEPERIODER_FORESPURT } from '../actions/oppfolgin
 export function* hentOppfolgingstilfelleperioder(action) {
     yield put(actions.hentOppfolgingstilfelleperioderHenter(action.orgnummer));
     try {
-        const data = yield call(get, `${window.APP_SETTINGS.REST_ROOT}/sykeforloep/oppfolgingstilfelleperioder?fnr=${action.fnr}&orgnummer=${action.orgnummer}`);
+        const data = yield call(get, `${window.APP_SETTINGS.REST_ROOT}/oppfolgingstilfelleperioder?fnr=${action.fnr}&orgnummer=${action.orgnummer}`);
         yield put(actions.hentOppfolgingstilfelleperioderHentet(data, action.orgnummer));
     } catch (e) {
         log(e);
