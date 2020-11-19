5fe21c3cdda4ce8d84fcc86147028c3ff603f2d1
Mathias Rørvik <mathias.fris.rorvik@nav.no>
2020-11-19 15:04:42 +0100

    alertstripe with text

--- src/containers/VedtakContainer.tsx
+++ src/containers/VedtakContainer.tsx
@@ -1,21 +1,9 @@
 import * as React from 'react';
-import {
-    useEffect,
-    useState,
-} from 'react';
-import {
-    useDispatch,
-    useSelector,
-} from 'react-redux';
-import {
-    Column,
-    Row,
-} from 'nav-frontend-grid';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Column, Row } from 'nav-frontend-grid';
 import Panel from 'nav-frontend-paneler';
-import {
-    Undertekst,
-    Undertittel,
-} from 'nav-frontend-typografi';
+import { Undertekst, Undertittel } from 'nav-frontend-typografi';
 import { hentVedtak } from '../actions/vedtak_actions';
 import Side from '../sider/Side';
 import { VedtakDTO } from '../reducers/vedtak';
@@ -23,13 +11,22 @@ import { restdatoTildato } from '../utils/datoUtils';
 import VedtakInfopanel from '../components/vedtak/VedtakInfopanel';
 import styled from 'styled-components';
 import { VEDTAK } from '../enums/menypunkter';
+import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 
 const texts = {
     pageTitle: 'Vedtak',
+    comingSoon: `
+       Dette er en tidlig versjon av vedtakshistorikken. 
+       Feil og mangler kan forekomme. Maksdatoen er på vei! Bruk Yammer til å komme med forslag og Porten til å melde feil. 
+    `,
 };
 
 const StyledPanel = styled(Panel)`
     margin-bottom: .5em;
+`;
+
+const StyledAlertStripe = styled(AlertStripeFeil)`
+    margin: 0 .5em .5em .5em;
 `;
 
 const VedtakContainer = () => {
@@ -52,23 +49,27 @@ const VedtakContainer = () => {
 
     return (
         <Side fnr={fnr} tittel={texts.pageTitle} aktivtMenypunkt={VEDTAK}>
-            {vedtak && selectedVedtak && <Row>
-                <Column className="col-xs-5">
-                    {vedtak.data.map((v: VedtakDTO) => (
-                        <StyledPanel>
-                            <Undertittel>
-                                {restdatoTildato(v.vedtak.fom)} - {restdatoTildato(v.vedtak.tom)}
-                            </Undertittel>
-                            <Undertekst>
-                                {`Vedtaksdato: ${restdatoTildato(v.opprettet)} · Restdager: ${v.vedtak.gjenståendeSykedager}`}
-                            </Undertekst>
-                        </StyledPanel>
-                    ))}
-                </Column>
-                <Column className="col-xs-7">
-                    {vedtak.data.map((vedtak: VedtakDTO) => <VedtakInfopanel selectedVedtak={vedtak}/>) }
-                </Column>
-            </Row>}
+            {vedtak && selectedVedtak &&
+            <>
+                <Row><StyledAlertStripe>{texts.comingSoon}</StyledAlertStripe></Row>
+                <Row>
+                    <Column className="col-xs-5">
+                        {vedtak.data.map((v: VedtakDTO) => (
+                            <StyledPanel>
+                                <Undertittel>
+                                    {restdatoTildato(v.vedtak.fom)} - {restdatoTildato(v.vedtak.tom)}
+                                </Undertittel>
+                                <Undertekst>
+                                    {`Vedtaksdato: ${restdatoTildato(v.opprettet)} · Restdager: ${v.vedtak.gjenståendeSykedager}`}
+                                </Undertekst>
+                            </StyledPanel>
+                        ))}
+                    </Column>
+                    <Column className="col-xs-7">
+                        {vedtak.data.map((vedtak: VedtakDTO) => <VedtakInfopanel selectedVedtak={vedtak}/>)}
+                    </Column>
+                </Row>
+            </>}
         </Side>);
 };
 
