6ad2d2e94f3a2df863a3e8eb235be6541ab3a835
Mathias Rørvik <mathias.fris.rorvik@nav.no>
2020-11-19 13:05:28 +0100

    fix test

--- test/reducers/ledereReducerTest.js
+++ test/reducers/ledereReducerTest.js
@@ -79,6 +79,7 @@ describe('ledere', () => {
         const action = { type: HENT_LEDERE_FEILET };
         const nextState = ledere(initialState, action);
         expect(nextState).to.deep.equal({
+            allLedere: [],
             henter: false,
             hentet: false,
             hentingFeilet: true,
