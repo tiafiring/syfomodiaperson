b41d886182b84c0b04cc76e3a16a8a090d265697
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-12-03 22:06:06 +0100

    Remove unused function

--- src/utils/reducerUtils.js
+++ src/utils/reducerUtils.js
@@ -7,10 +7,6 @@ export const harForsoktHentetMotebehov = (motebehovReducer) => {
   );
 };
 
-export const harForsoktHentetLedetekster = (ledeteksterReducer) => {
-  return ledeteksterReducer.hentet || ledeteksterReducer.hentingFeilet;
-};
-
 export const harForsoktHentetLedere = (ledereReducer) => {
   return ledereReducer.hentet || ledereReducer.hentingFeilet;
 };
