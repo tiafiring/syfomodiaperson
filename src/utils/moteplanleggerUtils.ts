c26d6cc35fb5f1ca2c3af6bead1e81a996f9d4d7
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-12-07 19:23:37 +0100

    Convert moteplanleggerUtils to Typescript

--- src/utils/moteplanleggerUtils.ts
+++ src/utils/moteplanleggerUtils.ts
@@ -1,24 +1,25 @@
 import { BRUKER, MULIGE_SVAR } from "../konstanter";
 
-export const brukerHarSvart = (svartidspunkt, created) => {
+export const brukerHarSvart = (svartidspunkt: any, created: any) => {
   if (!svartidspunkt) {
     return false;
   }
   return new Date(svartidspunkt) > new Date(created);
 };
 
-export const getSvar = ({ valgt, created }, svartidspunkt) => {
+export const getSvar = (deltakerSvar: any, svartidspunkt: any) => {
+  const { valgt, created } = deltakerSvar;
   if (!brukerHarSvart(svartidspunkt, created)) {
     return MULIGE_SVAR.IKKE_SVART;
   }
   return valgt ? MULIGE_SVAR.PASSER : MULIGE_SVAR.PASSER_IKKE;
 };
 
-export const getTidligereAlternativer = (mote, deltakertype = BRUKER) => {
-  const innloggetBruker = mote.deltakere.filter((deltaker) => {
+export const getTidligereAlternativer = (mote: any, deltakertype = BRUKER) => {
+  const innloggetBruker = mote.deltakere.filter((deltaker: any) => {
     return deltaker.type === deltakertype;
   })[0];
-  return mote.alternativer.filter((alternativ) => {
+  return mote.alternativer.filter((alternativ: any) => {
     return alternativ.created < innloggetBruker.svartidspunkt;
   });
 };
