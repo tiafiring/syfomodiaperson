db2d211b6a71364db870943723c2e1136756d5dd
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-12-08 15:27:49 +0100

    Fix TS-error in dokumentinfoReducer

--- src/data/oppfolgingsplan/dokumentinfo.ts
+++ src/data/oppfolgingsplan/dokumentinfo.ts
@@ -11,7 +11,7 @@ export interface VirksomhetState {
   hentet: boolean;
   hentingFeilet: boolean;
   hentingForsokt: boolean;
-  data: DokumentinfoDTO;
+  data: DokumentinfoDTO | {};
 }
 
 export interface DokumentinfoMapState {
