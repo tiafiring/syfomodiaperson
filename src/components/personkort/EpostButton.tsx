dcddf7908a532d43fa61d9e859335443debe0ffb
Mathias Rørvik <mathias.fris.rorvik@nav.no>
2020-12-16 14:07:09 +0100

    implement email icon that writes to clipboard

---
+++ src/components/personkort/EpostButton.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from "react";
+import { Column } from "nav-frontend-grid";
+import Popover from "nav-frontend-popover";
+import styled from "styled-components";
+
+const texts = {
+  kopiert: "Epost er kopiert!",
+};
+
+const StyledButton = styled.button`
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background: none;
+`;
+
+const StyledP = styled.p`
+  padding: 0.5em;
+`;
+
+interface EpostButtonProps {
+  epost?: string;
+}
+
+const EpostButton = ({ epost }: EpostButtonProps) => {
+  const [popoverAnker, setPopoverAnker] = useState<HTMLElement>();
+  const [show, setShow] = useState<boolean>(false);
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const togglePopover = () => {
+    if (popoverAnker) {
+      setPopoverAnker(undefined);
+    } else if (buttonRef.current) {
+      setPopoverAnker(buttonRef.current);
+
+      if (!show) {
+        setShow(true);
+      }
+
+      if (epost) {
+        navigator.clipboard.writeText(epost);
+      }
+    }
+  };
+
+  return (
+    <Column className="col-sm-4">
+      <StyledButton ref={buttonRef} onClick={togglePopover} value={epost}>
+        <img alt="epost" src="/sykefravaer/img/svg/epost.svg" />
+      </StyledButton>
+      <Popover
+        ankerEl={popoverAnker}
+        onRequestClose={() => setPopoverAnker(undefined)}
+      >
+        <StyledP>{texts.kopiert}</StyledP>
+      </Popover>
+    </Column>
+  );
+};
+
+export default EpostButton;
