401b23eb89b31b007a9542930458badad06da2b8
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-04 12:56:34 +0100

    Fix setup of jsdom for tests

--- test/setup.js
+++ test/setup.js
@@ -1,62 +1,48 @@
+import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { configure } from "enzyme";
 import path from "path";
-import jsdom from "jsdom";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const dotEnvPath = path.resolve(".env");
+
+require("dotenv").config({
+  path: dotEnvPath,
+});
+
+const { JSDOM } = require("jsdom");
+
+const jsdom = new JSDOM("<!doctype html><html><body></body></html>");
+const { window } = jsdom;
 
 function copyProps(src, target) {
   const props = Object.getOwnPropertyNames(src)
-    .filter((prop) => typeof target[prop] === "undefined")
-    .map((prop) => Object.getOwnPropertyDescriptor(src, prop));
+    .filter((prop) => {
+      return typeof target[prop] === "undefined";
+    })
+    .map((prop) => {
+      return Object.getOwnPropertyDescriptor(src, prop);
+    });
   Object.defineProperties(target, props);
 }
 
-function setUpDomEnvironment() {
-  const { JSDOM } = jsdom;
-  const dom = new JSDOM("<!doctype html><html><body></body></html>", {
-    url: "http://localhost/",
-  });
-  const { window } = dom;
+let temp = null;
+const localS = {
+  getItem() {
+    return temp;
+  },
+  setItem(key, value) {
+    temp = value;
+  },
+};
 
-  let temp = null;
-  const localS = {
-    getItem(key) {
-      return temp;
-    },
-    setItem(key, value) {
-      temp = value;
-    },
-  };
+global.HTMLElement = window.HTMLElement;
+global.localStorage = localS;
+global.XMLHttpRequest = window.XMLHttpRequest;
 
-  global.localStorage = localS;
-  global.window = window;
-  global.document = window.document;
-  global.navigator = {
-    userAgent: "node.js",
-  };
-
-  copyProps(window, global);
-
-  const dotEnvPath = path.resolve(".env");
-
-  require("dotenv").config({
-    path: dotEnvPath,
-  });
-}
-
-propagateToGlobal(window);
-
-function propagateToGlobal(window) {
-  for (const key in window) {
-    if (!window.hasOwnProperty(key)) {
-      continue;
-    }
-
-    if (key in global) {
-      continue;
-    }
-    global[key] = window[key];
-  }
-}
-
-setUpDomEnvironment();
-configure({ adapter: new Adapter() });
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: "node.js",
+};
+copyProps(window, global);
