2e67d7b7faf7c86383a3b72beaf003a3daadb8d4
Anders Rognstad <anders.rognstad@nav.no>
2021-06-08 12:31:32 +0200

    Remove prod less module loader

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,40 +1,7 @@
 import { merge } from "webpack-merge";
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 const common = require("./webpack.common.ts");
 
 module.exports = merge(common, {
   mode: "production",
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: "css-loader",
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [["postcss-preset-env"]],
-              },
-            },
-          },
-          {
-            loader: "less-loader",
-          },
-        ],
-      },
-    ],
-  },
 });
