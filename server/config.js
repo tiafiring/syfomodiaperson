4035ac7c44080dddb4f02acfb19986a4742bedc3
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-08-13 11:24:11 +0200

    Add appname to proxy config

--- server/config.js
+++ server/config.js
@@ -29,86 +29,103 @@ const auth = {
   }),
 
   fastlegerest: {
+    applicationName: "fastlegerest",
     host: envVar({
       name: "FASTLEGEREST_HOST",
     }),
   },
   flexInternGateway: {
+    applicationName: "flex-intern-gateway",
     host: envVar({
       name: "FLEX_INTERN_GATEWAY_HOST",
     }),
   },
   isdialogmote: {
+    applicationName: "isdialogmote",
     host: envVar({
       name: "ISDIALOGMOTE_HOST",
     }),
   },
   ispengestopp: {
+    applicationName: "ispengestopp",
     host: envVar({
       name: "ISPENGESTOPP_HOST",
     }),
   },
   ispersonoppgave: {
+    applicationName: "ispersonoppgave",
     host: envVar({
       name: "ISPERSONOPPGAVE_HOST",
     }),
   },
   isprediksjon: {
+    applicationName: "isprediksjon",
     host: envVar({
       name: "ISPREDIKSJON_HOST",
     }),
   },
   modiacontextholder: {
+    applicationName: "modiacontextholder",
     host: envVar({
       name: "MODIACONTEXTHOLDER_HOST",
     }),
   },
   modiasyforest: {
+    applicationName: "modiasyforest",
     host: envVar({
       name: "MODIASYFOREST_HOST",
     }),
   },
   syfobehandlendeenhet: {
+    applicationName: "syfobehandlendeenhet",
     host: envVar({
       name: "SYFOBEHANDLENDEENHET_HOST",
     }),
   },
   syfomoteadmin: {
+    applicationName: "syfomoteadmin",
     host: envVar({
       name: "SYFOMOTEADMIN_HOST",
     }),
   },
   syfomotebehov: {
+    applicationName: "syfomotebehov",
     host: envVar({
       name: "SYFOMOTEBEHOV_HOST",
     }),
   },
   syfooppfolgingsplanservice: {
+    applicationName: "syfooppfolgingsplanservice",
     host: envVar({
       name: "SYFOOPPFOLGINGSPLANSERVICE_HOST",
     }),
   },
   syfoperson: {
+    applicationName: "syfoperson",
     host: envVar({
       name: "SYFOPERSON_HOST",
     }),
   },
   syfosmregister: {
+    applicationName: "syfosmregister",
     host: envVar({
       name: "SYFOSMREGISTER_HOST",
     }),
   },
   syfosoknad: {
+    applicationName: "syfosoknad",
     host: envVar({
       name: "SYFOSOKNAD_HOST",
     }),
   },
   syfotilgangskontroll: {
+    applicationName: "syfo-tilgangskontroll",
     host: envVar({
       name: "SYFOTILGANGSKONTROLL_HOST",
     }),
   },
   syfoveileder: {
+    applicationName: "syfoveileder",
     host: envVar({
       name: "SYFOVEILEDER_HOST",
     }),
