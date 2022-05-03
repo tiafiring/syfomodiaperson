c703540d80479d63b6a5af2ef28754d5f62c2be8
Erik Jans <erik.gunnar.jansen@nav.no>
2022-05-03 09:43:39 +0200

    Refactor MockUnleash (#680)
    Reuse string constants for unleash toggles in mock.

--- mock/unleash/mockUnleash.ts
+++ mock/unleash/mockUnleash.ts
@@ -1,13 +1,17 @@
 //Enable everything for local development
 import { UNLEASH_ROOT } from "../../src/apiConstants";
+import { ToggleNames } from "../../src/data/unleash/unleash_types";
 
 export const mockUnleash = (server) => {
+  const unleashToggles = Object.values(ToggleNames).reduce(
+    (accumulator, toggleName) => {
+      return { ...accumulator, [toggleName]: true };
+    },
+    {}
+  );
+
   server.post(`${UNLEASH_ROOT}/*`, (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    res.send(
-      JSON.stringify({
-        "syfo.syfomodiaperson.sykmeldingsgrad": true,
-      })
-    );
+    res.send(JSON.stringify(unleashToggles));
   });
 };
