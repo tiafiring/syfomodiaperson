ff6852df72e80cdb7dc630be599b2beb86cb6512
Erik Jans <erik.gunnar.jansen@nav.no>
2022-05-24 16:19:11 +0200

    Add packages for type for redis and connect-redis (#701)

--- server/session.ts
+++ server/session.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
 import express = require("express");
 import connectRedis = require("connect-redis");
 import session = require("express-session");
